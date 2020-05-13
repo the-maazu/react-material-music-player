48a96cbb740acca22fb25d725bb06fc5cf1c2d12
maazu <hassanmaazu@gmail.com>
2020-05-13 23:12:06 +0000

    converted TrackDetails to function componennt

--- src/components/Player/TrackDetials.js
+++ src/components/Player/TrackDetials.js
@@ -1,52 +1,31 @@
 import React, { useState } from 'react'
-import { withStyles} from '@material-ui/core/styles';
+import { makeStyles} from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography'
 
 import ScrollingText from './ScrollingText.js'
 
-const styles = {
-    root: {
-        overflow: 'hidden'
-    }
-  };
 
-class TrackDetails extends React.Component{
+const useStyles = makeStyles({
+        root: {
+            overflow: 'hidden'
+        }
+    })
 
-    constructor(props){
-        super(props);
+export default function TrackDetails(props){
 
-        this.rootRef = React.createRef();
-        this.trackTitleRef = React.createRef();
-        this.artistRef = React.createRef()
+    const classes = useStyles();
 
-        this.props = props
-    }
+    const showArtist = props.showArtist;
 
-    render(){
-        
-        const {
-            classes,
-            showArtist
-        } = this.props;
+    return (
+        <div 
+        className={classes.root}>
 
-        return (
-            <div 
-            ref={this.rootRef}
-            className={classes.root}>
+            <ScrollingText text="Killing Me Softly"/>
 
-                <ScrollingText text="Killing Me Softly"/>
-
-                {false ? 
-                <ScrollingText text="Killest Mikestic"/> : null
-                }
-            </div>
-        )
-    }
+            {showArtist ? 
+            <ScrollingText text="Killest Mikestic"/> : null
+            }
+        </div>
+    )
 }
-
-TrackDetails.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
-
-  export default withStyles(styles)(TrackDetails);
