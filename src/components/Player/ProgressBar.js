5e4abdf2cdf3ff436fd7ae02b297c8a93f7bbb02
maazu <hassanmaazu@gmail.com>
2020-05-16 11:14:11 +0000

    progress slider and track time spacing

--- src/components/Player/ProgressBar.js
+++ src/components/Player/ProgressBar.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react'
 import Slider from '@material-ui/core/Slider';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography'
 
 const useStyles = makeStyles(theme => ({
+    root:{
+        margin: theme.spacing()
+    },
     slider: {
         width: '60vw'
     }
@@ -21,11 +25,19 @@ export default function(){
         alignItems="center"
         className={classes.root}
         >
-            <Grid item>0:00</Grid>
+            <Grid item>
+                <Typography>
+                    0:00
+                </Typography>
+            </Grid>
             <Grid item className={classes.slider}>
                 <Slider aria-labelledby="continuous-slider" />
             </Grid>
-            <Grid item>0:00</Grid>
+            <Grid item>
+                <Typography>
+                    0:00
+                </Typography>
+            </Grid>
         </Grid>
     )
 }
