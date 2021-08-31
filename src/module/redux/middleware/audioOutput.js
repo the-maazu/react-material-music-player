f729bdc37c85d9f66f5bbff9054a24c46accf90f
maazu <hassanmaazu@gmail.com>
2021-08-31 22:07:05 +0200

    audioOutput: ui time updates dispatch on timeupdate event, only continue play after track change if already playing

--- src/module/redux/middleware/audioOutput.js
+++ src/module/redux/middleware/audioOutput.js
@@ -1,39 +1,27 @@
 import actionTypes from '../actionTypes.js'
 import actionCreators from '../actionCreators.js'
+import { MediaStates } from '../store.js';
 
 const audioElement = new Audio();
 
 const audioOutput = (store) => {
 
-    let progressWorker
-
-    function setProgressWorker(){
-        
-        // clear before generating new worker
-        window.clearInterval(progressWorker)
-
-        progressWorker = window.setInterval(
-            () => {
-                // update current time
-                store.dispatch(
-                    actionCreators.setCurrentTime(
-                        Math.floor(audioElement.currentTime)
-                    )
-                )
-                // update time left
-                store.dispatch(
-                    actionCreators.setTimeLeft(
-                        Math.floor(audioElement.duration - audioElement.currentTime)
-                    )
-                )
-            },
-            1000 // 1s interval
+    audioElement.addEventListener('timeupdate', () => {
+        // update current time
+        store.dispatch(
+            actionCreators.setCurrentTime(
+                Math.floor(audioElement.currentTime)
+            )
         )
-    }
 
-    function clearProgressWorker(){
-        window.clearInterval(progressWorker)
-    }
+        // update time left
+        store.dispatch(
+            actionCreators.setTimeLeft(
+                Math.floor(audioElement.duration - audioElement.currentTime)
+            )
+        )
+
+    })
 
     // set default volume level
     audioElement.volume = store.getState().volume/100
@@ -45,24 +33,20 @@ const audioOutput = (store) => {
         
             switch(action.type){
                 case actionTypes.CHANGE_TRACK:
-                    // break if new source same as current source
-                    if(audioElement.src.includes(state.playlist[state.currentTrack].source))
-                    break
-                    audioElement.src = state.playlist[state.currentTrack].source
-                    audioElement.play()
+                    audioElement.src = state.playlist[action.payload.index].source
+                    if(state.mediaState === MediaStates.playing)
+                    audioElement.play() // continue playing if previous track was playing
                     break
                 case actionTypes.PLAY:
                     if( audioElement.src == "" )
                         audioElement.src = state.playlist[state.currentTrack].source
                     audioElement.play()
-                    setProgressWorker();
                     break
                 case actionTypes.PAUSE:
                     audioElement.pause()
                     break
                 case actionTypes.STOP:
                     audioElement.src = ""
-                    clearProgressWorker()
                     break
                 case actionTypes.SEEK:
                     audioElement.currentTime = action.payload.time;
