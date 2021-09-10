c42340d8a6be25370d05591d9295fa89d4e48c0b
maazu <hassanmaazu@gmail.com>
2021-09-10 18:02:09 +0200

    audioOutput: fixes #1, fixes #2, added "canplay" and "ended listeners"

--- src/module/redux/middleware/audioOutput.js
+++ src/module/redux/middleware/audioOutput.js
@@ -7,20 +7,41 @@ const audioElement = new Audio();
 const audioOutput = (store) => {
 
     audioElement.addEventListener('timeupdate', () => {
-        // update current time
+
+        //set current time updater
         store.dispatch(
             actionCreators.setCurrentTime(
                 Math.floor(audioElement.currentTime)
             )
         )
 
-        // update time left
+        // set time left updater
         store.dispatch(
             actionCreators.setTimeLeft(
                 Math.floor(audioElement.duration - audioElement.currentTime)
             )
         )
 
+        // set canplay listener incase there is buffering
+        audioElement.addEventListener('canplay', ()=>{
+            let mediaState = store.getState().mediaState
+            if(mediaState === MediaStates.playing)
+            audioElement.play()
+        })
+
+        // skip to next track after playback ends
+        audioElement.addEventListener('ended', ()=>{
+
+            // check if fully ended
+            if(audioElement.currentTime !== audioElement.duration)
+            return
+
+            let currentIndex = store.getState().currentTrack
+            let mediaState = store.getState().mediaState
+
+            if(mediaState === MediaStates.playing)
+            store.dispatch(actionCreators.changeTrack(++currentIndex))
+        })
     })
 
     // set default volume level
@@ -28,38 +49,52 @@ const audioOutput = (store) => {
 
     return (next) => (action) => {
 
-            let results = next(action);
-            let state = store.getState();
-        
-            switch(action.type){
-                case actionTypes.CHANGE_TRACK:
-                    audioElement.src = state.playlist[action.payload.index].source
-                    if(state.mediaState === MediaStates.playing)
-                    audioElement.play() // continue playing if previous track was playing
-                    break
-                case actionTypes.PLAY:
-                    if( audioElement.src === "" )
-                        audioElement.src = state.playlist[state.currentTrack].source
-                    audioElement.play()
-                    break
-                case actionTypes.PAUSE:
-                    audioElement.pause()
-                    break
-                case actionTypes.STOP:
-                    audioElement.src = ""
-                    break
-                case actionTypes.SEEK:
-                    audioElement.currentTime = action.payload.time;
-                    break
-                case actionTypes.CHANGE_VOLUME:
-                    audioElement.volume = action.payload.volume/100;
-                    break
-                default:
-                    break
-            }
-        
-            return results;
+        let state = store.getState();
+    
+        switch(action.type){
+            case actionTypes.CHANGE_TRACK:
+                let currentTrack = state.playlist[state.currentTrack]
+                let newTrack = state.playlist[action.payload.index]
+
+                if(currentTrack.ID === newTrack.ID) // if same track do nothing
+                break
+
+                audioElement.src = state.playlist[action.payload.index].source
+                if(
+                    state.mediaState === MediaStates.playing 
+                    && audioElement.readyState >= 2 ) // buffered enough to start playing
+                audioElement.play() // continue playing if previous track was playing
+                break
+
+            case actionTypes.PLAY:
+                if( audioElement.src === "" )
+                    audioElement.src = state.playlist[state.currentTrack].source
+                audioElement.play()
+                break
+
+            case actionTypes.PAUSE:
+                audioElement.pause()
+                break
+
+            case actionTypes.STOP:
+                audioElement.src = ""
+                break
+
+            case actionTypes.SEEK:
+                if( audioElement.src !== "")
+                audioElement.currentTime = action.payload.time;
+                break
+
+            case actionTypes.CHANGE_VOLUME:
+                audioElement.volume = action.payload.volume/100;
+                break
+
+            default:
+                break
         }
+    
+        return next(action);
+    }
 
 }
 
