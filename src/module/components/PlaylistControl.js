525617d2fb4597e4e4eee581cece66c265e3c3c3
maazu <hassanmaazu@gmail.com>
2021-10-02 19:13:37 +0200

    migrate Playlist components to muiv5

--- src/module/components/PlaylistControl.js
+++ src/module/components/PlaylistControl.js
@@ -3,172 +3,100 @@ import React,{useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import actionCreators from '../redux/actionCreators.js';
 
-import { makeStyles } from '@material-ui/core/styles';
-import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
-import ToggleButton from "@material-ui/lab/ToggleButton";
-import ShuffleIcon from "@material-ui/icons/Shuffle";
-import PlaylistIcon from "@material-ui/icons/QueueMusic";
-import Collapse from '@material-ui/core/Collapse';
-import ReactDraggableList from 'react-draggable-list';
-import Paper from '@material-ui/core/Paper'
-import Popover from '@material-ui/core/Popover';
+import { Shuffle as ShuffleIcon, QueueMusic as PlaylistIcon} from "@mui/icons-material/";
+import { Popover, Collapse, Box, ToggleButton} from '@mui/material/';
 
-import PlaylistItemTemplate from './PlaylistItemTemplate.js'
-import { shallowEqual } from 'react-redux';
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        position: isDesktop=> isDesktop? 'relative': null,
-        bottom: isDesktop=> isDesktop? 0 : null
-    },
-    draggablelistContainer: {
-        overflow: 'auto',
-        height: isDesktop => isDesktop ? '50vh' :'60vh',
-        width: isDesktop => isDesktop ? '25vw' : null,
-        margin: theme.spacing(),
-    },
-    buttonGroup: {
-        width: '100%',
-    },
-    button: {
-        width: '50%'
-    }
-}));
+import Playlist from './Playlist.js';
 
 export default function PlaylistControl(props){
+    
+    const sx = props.sx
+    const playlistViewMode = props.playlistViewMode
 
-    const isDesktop = props.isDesktop
-
-    const {
-        playlist,
-        shuffled,
-        currentTrack
-    } = useSelector(
-        ({
-            playlist,
-            shuffled,
-            currentTrack
-        }) => ({
-            playlist,
-            shuffled,
-            currentTrack
-        }),
-        shallowEqual
-    )
-
-    const dispatch = useDispatch();
-    const onReorder = (newList, newIndex) =>{
-        dispatch(actionCreators.updatePlaylist(newList))
-        dispatch(actionCreators.changeTrack(newIndex))
-    }
-    const onShuffle = (bool) => dispatch(actionCreators.shuffle(bool))
-    const onTrackSelect = (index) => dispatch(actionCreators.changeTrack(index))
-
-    const classes = useStyles(isDesktop);
-    const [values, setValues] = useState(shuffled ? ['shuffle']: []);
-    const [expanded, expand] = useState(false);
+    const shuffled = useSelector( (state) => state.shuffled)
+    const [playlistVisible, showPlaylist] = useState(false)
     const [anchorEl, setAnchor] = useState(null)
 
-    const handleChange = (event, newValues) => {
-        event.stopPropagation()
-        
-        setValues(newValues);
-        if (Boolean(newValues.find( element => element === 'show-playlist' ))){
-            setAnchor(event.target.parentElement.parentElement)
-            expand(true);
-        }else expand(false)
+    const dispatch = useDispatch();
+    const onShuffle = (bool) => dispatch(actionCreators.shuffle(bool))
 
-        if (Boolean(newValues.find( element => element === 'shuffle' ))){
-            onShuffle(true)
-        }else onShuffle(false)
-    };
-
-    const handlePopoverClose = (event) => {
-        const newValues = values.filter(value => value !== 'show-playlist')
-        setValues(newValues);
+    const handlePopoverClose = () => {
+        showPlaylist(false);
         setAnchor(null)
-        expand(false);
     };
+    
+    const buttonsContainerRef = React.useRef();
 
-    const draggablelistContainerRef = React.createRef();
-    const reactDraggableList = (
-        <Paper
-        elevation='0'
-        className={classes.draggablelistContainer}
-        ref={draggablelistContainerRef}>
-            <ReactDraggableList 
-            list={playlist}
-            itemKey='ID'
-            template={PlaylistItemTemplate}
-            onMoveEnd={(newList)=> {
-                onReorder(
-                    newList, 
-                    newList.findIndex(
-                        (track) => {
-                            return track.ID === playlist[currentTrack].ID
-                        }
-                    )
-                )}
-            }
-            container={()=> draggablelistContainerRef.current }
-            commonProps={{
-                listOfID: playlist.map((element) => element.ID),
-                currentTrackID: playlist[currentTrack].ID,
-                onTrackSelect: onTrackSelect
-            }}
-            />
-        </Paper>
-    )
-                                
     return (
-        <div className={classes.root}>
-
-            {isDesktop?
-            <Popover
-            open={Boolean(anchorEl)}
-            anchorEl={anchorEl}
-            onClose={handlePopoverClose}
-            anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              transformOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-              }}
-            >
-                {reactDraggableList}
-            </Popover>
-            :
-            <Collapse
-            collapsedHeight={'0'}
-            in={expanded}
-            >
-                {reactDraggableList}
-            </Collapse>}
-
-            <ToggleButtonGroup
-            className={classes.buttonGroup}
-            position='center' 
-            value={values}
-            onChange={handleChange}
+        <Box sx={{
+            display:'flex',
+            flexDirection: 'column-reverse',
+            alignItems: 'center',
+            ...sx
+        }}>
+            <Box
+                ref={buttonsContainerRef}
+                sx={{
+                    width: '100%',
+                    display:'flex',
+                    flexDirection: 'row',
+                    "& > .children":{
+                        mx:0.5,
+                        flexGrow: 1
+                    }
+                }}
             >
                 <ToggleButton 
-                value='shuffle'
-                className={classes.button}
+                    className="children"
+                    value='shuffle' 
+                    selected={shuffled}
+                    onChange={() => {   
+                        onShuffle(!shuffled);
+                    }}
                 >
                     <ShuffleIcon/>
                 </ToggleButton>
 
-                <ToggleButton 
-                value='show-playlist'
-                className={classes.button}
+                <ToggleButton
+                    className="children"
+                    value='show playlist'
+                    selected={playlistVisible}
+                    onChange={() => {
+                        setAnchor(buttonsContainerRef.current)
+                        showPlaylist(!playlistVisible);
+                    }}
                 >
                     <PlaylistIcon/>
                 </ToggleButton>
-
-            </ToggleButtonGroup>
-
-        </div>
-    )
+            </Box>
+            
+            { playlistViewMode === 'popover' ?
+                <Popover
+                    open={Boolean(anchorEl)}
+                    anchorEl={anchorEl}
+                    onClose={handlePopoverClose}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    transformOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                    }}
+                >
+                    <Playlist sx={{
+                        width: '30vw',
+                        height: '60vh'
+                    }}/>
+                </Popover>
+                    :
+                <Collapse collapsedSize={'0'} in={playlistVisible}>
+                    <Playlist sx={{
+                        height: '60vh',
+                        width: '90vw'
+                    }}/>
+                </Collapse>
+            }
+        </Box>
+    );
 }
--- src/module/components/PlaylistItemTemplate.js
+++ src/module/components/PlaylistItemTemplate.js
@@ -2,92 +2,73 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-import { withStyles } from '@material-ui/core/styles';
-import PlayIcon from "@material-ui/icons/PlayArrowRounded";
-import Typography from "@material-ui/core/Typography"
-import Grid from "@material-ui/core/Grid"
-import ReorderIcon from '@material-ui/icons/Menu'
+import { PlayArrowRounded as PlayIcon, Menu as ReorderIcon } from "@mui/icons-material/";
+import Box from "@mui/material/Box"
 
 import CoverArt from './CoverArt';
 import withoutPropagation from '../utils/withoutPropagation';
-
-const styles = theme => ({
-    root: {
-        maxHeight:'100%',
-        border: `1px solid ${theme.palette.divider}`,
-        borderRadius: theme.shape.borderRadius,
-        padding: theme.spacing(),
-        '& > *':{
-            marginRight: theme.spacing()
-        }
-    },
-    coverArt: {
-        height:'50px'
-    },
-    nowPlayingIconContainer: {
-        position: 'absolute', 
-        top: 0, 
-        left: 0,
-        height: '100%', 
-        width: '100%'
-    }
-  });
 
 class PlaylistItemTemplate extends React.Component {
 
     constructor(props){
         super(props);
-
         this.props = props
-
     }
 
-
     render() {
-
         const {
-
             item,
             dragHandleProps,
             commonProps,
-            classes
-
         } = this.props;
 
         return (
-            <Grid 
-            container
-            direction='row'
-            justifyContent='space-between'
-            alignItems='center'
-            wrap='nowrap'
-            className={classes.root}
-            onClick={
-                withoutPropagation(
-                    commonProps.onTrackSelect, commonProps.listOfID.findIndex((ID) => ID === item.ID)
-                )
-            }
+            <Box sx={{
+                    maxHeight: '100%',// requirement for best results
+                    // flex box
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-around',
+                    alignItems: 'center',
+                    flexWrap: 'nowrap',
+                    // border
+                    border: 1,
+                    borderColor:'divider',
+                    borderRadius: 1,
+                    // paddding
+                    boxSizing: "border-box",
+                    padding: 1,
+                    // bgColor
+                    bgcolor: 'background.paper'
+                }}
+                onClick={
+                    withoutPropagation(
+                        commonProps.onTrackSelect,
+                        commonProps.listOfID.findIndex((ID) => ID === item.ID)
+                    )
+                }
             >
-                {Boolean(commonProps.currentTrackID === item.ID) ? <PlayIcon item /> : null}
+                {/*render now playing icon or empty box matching icon size */}
+                { commonProps.currentTrackID === item.ID ? 
+                    <PlayIcon/> : <Box sx={{width:'24px',height:'24px'}}/>
+                }
                 
-                <CoverArt item src={item.coverArt} size='50px'/>
+                <CoverArt 
+                    src={item.coverArt} 
+                    sx={{
+                        height:'48px',
+                        width:'48px'
+                    }}
+                />
                 
-                <Grid item xs={9}>
-                    <Typography variant='subtitle1'>
-                        {item.title}
-                    </Typography>
-                    <Typography variant='subtitle2'>
-                        {item.artist}
-                    </Typography>
-                </Grid>
+                <Box sx={{flexGrow: 1, mx: 1}}>
+                    <Box sx={{ typography: 'subtitl3' }}>{item.title}</Box>
+                    <Box sx={{ typography: 'subtitle2' }}>{item.artist}</Box>
+                </Box>
                 
-                <Grid item xs={1}>
-                    <ReorderIcon 
-                    item {...dragHandleProps}
-                    onClick={(e) => {e.stopPropagation()}}/>
-                </Grid>
-            </Grid>
-        )
+                <ReorderIcon item {...dragHandleProps} onClick={(e) => {e.stopPropagation()}}/>
+            </Box>
+        );
     }
 }
 
@@ -95,4 +76,4 @@ PlaylistItemTemplate.propTypes = {
     classes: PropTypes.object.isRequired,
   };
 
-export default withStyles(styles)(PlaylistItemTemplate);
+export default (PlaylistItemTemplate);
