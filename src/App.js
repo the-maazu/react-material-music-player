89559c2b7757421805f153e7baf7e408991dd2c8
maazu <hassanmaazu@gmail.com>
2021-10-06 00:20:15 +0200

    add theme and update demo

--- src/App.js
+++ src/App.js
@@ -2,40 +2,231 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import {
+  Box,
+  ToggleButtonGroup,
+  ToggleButton,
+  useMediaQuery,
+  TextField,
+  Link,
+} from "@mui/material";
+import {
+  LightModeRounded,
+  ComputerRounded,
+  DarkModeRounded,
+} from "@mui/icons-material";
+
 // To use the player this is what you need to import
 import Player, { TrackModel, PlayerInterface } from "./module/index.js";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const theme = createTheme();
+import makeTheme from "./makeTheme";
 
 function App() {
+  const [mode, setMode] = React.useState("system");
+  const isDark = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = makeTheme(
+    mode === "system" ? (isDark ? "dark" : "light") : mode
+  );
+
+  const [
+    {
+      width,
+      position,
+      bottom,
+      boxShadow,
+      borderRadiusTL,
+      borderRadiusTR,
+      borderRadiusBL,
+      borderRadiusBR,
+    },
+    setSx,
+  ] = React.useState({
+    width: "100vw",
+    position: "fixed",
+    bottom: 0,
+    boxShadow: 8,
+    borderRadiusTL: 1,
+    borderRadiusTR: 1,
+    borderRadiusBL: 0,
+    borderRadiusBR: 0,
+  });
+
+  const handleForm = (e) => {
+    const id = e.target.id;
+    const value = e.target.value;
+
+    setSx({
+      width,
+      position,
+      bottom,
+      boxShadow,
+      borderRadiusTL,
+      borderRadiusTR,
+      borderRadiusBL,
+      borderRadiusBR,
+      [`${id}`]: value,
+    });
+  };
+
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>react-material-music-player</p>
-          <a
-            className="App-link"
-            href="https://github.com/the-maazu/react-material-music-player"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-          <a
-            className="App-link"
+      <Box
+        sx={{
+          width: "100vw",
+          height: "100vh",
+          bgcolor: "background.paper",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "text.primary",
+          textAlign: "center",
+        }}
+      >
+        <img src={logo} className="App-logo" alt="logo" />
+        <Box sx={{ typography: "h6" }}>react-material-music-player</Box>
+
+        <div>
+          <Link
             href="https://www.npmjs.com/package/react-material-music-player"
             target="_blank"
             rel="noopener noreferrer"
           >
-            npmjs
-          </a>
-        </header>
+            npm
+          </Link>
+          <br />
+          <Link
+            href="https://github.com/the-maazu/react-material-music-player"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github
+          </Link>
+        </div>
 
-        {/* simply render player*/}
-        <Player />
-      </div>
+        <Box sx={{ typography: "body3", margin: 2 }}>
+          Resize window to eneter mobile mode. <br />
+          Swipe up or tap player to expand in mobile mode
+        </Box>
+
+        <ToggleButtonGroup
+          value={mode}
+          exclusive={true}
+          onChange={(e, value) => {
+            if (value) setMode(value);
+          }}
+        >
+          <ToggleButton value={"light"}>
+            <LightModeRounded />
+          </ToggleButton>
+          <ToggleButton value={"system"}>
+            <ComputerRounded />
+          </ToggleButton>
+          <ToggleButton value={"dark"}>
+            <DarkModeRounded />
+          </ToggleButton>
+        </ToggleButtonGroup>
+
+        <Box sx={{ display: "flex", margin: 2 }}>
+          <Box sx={{ margin: 1 }}>
+            <TextField
+              sx={{ display: "block" }}
+              margin={"dense"}
+              size="small"
+              id="width"
+              label="width"
+              value={width}
+              onChange={handleForm}
+            />
+            <TextField
+              sx={{ display: "block" }}
+              margin={"dense"}
+              size="small"
+              id="position"
+              label="position"
+              value={position}
+              onChange={handleForm}
+              type="text"
+            />
+            <TextField
+              sx={{ display: "block" }}
+              margin={"dense"}
+              size="small"
+              id="bottom"
+              label="bottom"
+              value={bottom}
+              onChange={handleForm}
+              type="text"
+            />
+            <TextField
+              sx={{ display: "block" }}
+              margin={"dense"}
+              size="small"
+              id="boxShadow"
+              label="boxShadow"
+              value={boxShadow}
+              onChange={handleForm}
+              type="number"
+            />
+          </Box>
+
+          <Box sx={{ margin: 1 }}>
+            <TextField
+              sx={{ display: "block" }}
+              margin={"dense"}
+              size="small"
+              id="borderRadiusTL"
+              label="borderRadiusTL"
+              value={borderRadiusTL}
+              onChange={handleForm}
+              type="number"
+            />
+            <TextField
+              sx={{ display: "block" }}
+              margin={"dense"}
+              size="small"
+              id="borderRadiusTR"
+              label="borderRadiusTR"
+              value={borderRadiusTR}
+              onChange={handleForm}
+              type="number"
+            />
+            <TextField
+              sx={{ display: "block" }}
+              margin={"dense"}
+              size="small"
+              id="borderRadiusBL"
+              label="borderRadiusBL"
+              value={borderRadiusBL}
+              onChange={handleForm}
+              type="number"
+            />
+            <TextField
+              sx={{ display: "block" }}
+              margin={"dense"}
+              size="small"
+              id="borderRadiusBR"
+              label="borderRadiusBR"
+              value={borderRadiusBR}
+              onChange={handleForm}
+              type="number"
+            />
+          </Box>
+        </Box>
+
+        <Player
+          // disableDrawer
+          sx={{
+            width: width,
+            position: position,
+            bottom: bottom,
+            boxShadow: boxShadow,
+            borderRadius: `${borderRadiusTL}px ${borderRadiusTR}px ${borderRadiusBL}px ${borderRadiusBR}px`,
+          }}
+        />
+      </Box>
     </ThemeProvider>
   );
 }
@@ -47,7 +238,7 @@ const TEST_MEDIA =
 // update playlist with test data and start play
 PlayerInterface.play([
   new TrackModel(
-    1,
+    "1",
     TEST_MEDIA + "bach.jpg",
     "68 Choral",
     "Bach",
@@ -55,12 +246,13 @@ PlayerInterface.play([
   ),
 ]);
 
-// wait 3 seconds then adds music at end of playlist
+// wait 3 seconds
 window.setTimeout(
   () =>
+    // adds music at end of playlist
     PlayerInterface.playLater([
       new TrackModel(
-        2,
+        "2",
         TEST_MEDIA + "emerson.jpeg",
         "All through the night",
         "Emerson",
@@ -71,12 +263,13 @@ window.setTimeout(
   3000 // 3 seconds
 );
 
-// wait 6 seconds then add music after current track
+// wait 6 seconds
 window.setTimeout(
   () =>
+    // add music after current track
     PlayerInterface.playNext([
       new TrackModel(
-        3,
+        "3",
         TEST_MEDIA + "guido.jpg",
         "Ut queant laxis",
         "Guido von Arezzo",
