59c1daefbbc5fc9ea2f2741840b52bdd8aebfe98
maazu <hassanmaazu@gmail.com>
2021-10-07 14:24:21 +0200

    fix and imporved track selection when reordering

--- src/module/redux/middleware/updatePlaylistHelper.js
+++ src/module/redux/middleware/updatePlaylistHelper.js
@@ -3,17 +3,42 @@ import actionTypes from "../actionTypes.js";
 
 const updatePlaylistHelper = (store) => {
   return (next) => (action) => {
-    let state = store.getState();
-    let currentTrack = state.currentTrack;
+    if (action.type !== actionTypes.UPDATE_PLAYLIST) return next(action);
 
-    // stop dispatch if current track index out of bound
-    if (
-      action.type === actionTypes.UPDATE_PLAYLIST &&
-      currentTrack >= action.payload.playlist.length
-    )
-      store.dispatch(actionCreators.changeTrack(0));
+    // store prev playlist and new playlist
+    const { playlist, currentTrack } = store.getState();
+    const newPlaylist = action.payload.playlist;
 
-    return next(action);
+    // update state with new playlist
+    const result = next(action);
+
+    // check if playlist changed
+    let playlistChanged = false;
+    if (newPlaylist.length !== playlist.length) playlistChanged = true;
+    else
+      for (let i = 0; i < playlist.length; i++) {
+        if (newPlaylist[i].ID !== playlist[i].ID) {
+          playlistChanged = true;
+          break;
+        }
+      }
+
+    // if playlist changed look for current track
+    if (playlistChanged) {
+      let newCurrentTrack = -1;
+      for (let i = 0; i < newPlaylist.length; i++) {
+        if (newPlaylist[i].ID === playlist[currentTrack].ID) {
+          newCurrentTrack = i;
+          break;
+        }
+      }
+
+      // if not found then its completely playlist, update current track to 0
+      if (newCurrentTrack === -1) store.dispatch(actionCreators.changeTrack(0));
+      else store.dispatch(actionCreators.changeTrack(newCurrentTrack)); // else update current track
+    }
+
+    return result;
   };
 };
 
